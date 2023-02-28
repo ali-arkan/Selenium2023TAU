ecb9a9cf4db4e65d88307c0c5a790cf06f7c071d
Ali Arkan <keep.alive.me@gmail.com>
2023-02-28 22:21:57 +0300

    test comments have been added

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -8,28 +8,6 @@ import org.testng.annotations.BeforeClass;
 
 public class BaseTests {
 
-    /*
-           Login Test Steps:
-           1. Click the Form Authentication url on the page
-           2. Fill username, pw on the page, then click login button
-           3. Verify the warning message on the opened page
-
-           Page Design:
-           MAIN
-              1. HomePage: find and click the url, login page will be opened
-              2. Login Page: set username, pw and click the login button, Secure page will be opened
-              3. SecureAreaPage: Text message will be verified
-           TEST
-              1. BaseTests : Webdriver defining and base url is opened
-              2. LoginTests: Click the url (homepage), set usrname,pw, click the button (Login Page) and Verify the text (SecureAreaPage)
-
-
-            DrowDown Test
-              1. Click the Dropdown page (click link on HomePage)
-              2. Select an option from dropdown and make sure we have selected
-            */
-
-
 
 
 
--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -11,11 +11,27 @@ public class LoginTests extends BaseTests {
      public void testSuccessfulLogin()
      {
 
+          /*
+           Page Design:
+           MAIN
+              1. HomePage: find and click the url, login page will be opened
+              2. Login Page: set username, pw and click the login button, Secure page will be opened
+              3. SecureAreaPage: Text message will be verified
+           TEST
+              1. BaseTests : Webdriver defining and base url is opened
+              2. LoginTests: Click the url (homepage), set usrname,pw, click the button (Login Page) and Verify the text (SecureAreaPage)
+            */
 
+
+         //1. Click the Form Authentication url on the page
          LoginPage loginPage= homePage.ClickformAuth();
+
+         // 2. Fill username, pw on the page, then click login button
          loginPage.setUsername("tomsmith");
          loginPage.setPassword("SuperSecretPassword!");
          SecureAreaPage securePages= loginPage.clickLoginButton();
+
+         //3. Verify the test message on the opened page
          assertTrue(securePages.getAlertText()
                  .contains("You logged into a secure area!"),
                  "alert text is incorrect");
