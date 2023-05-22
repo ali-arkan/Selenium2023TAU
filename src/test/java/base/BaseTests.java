00d402dae0c980bdf3836c81b92e5e6680348f24
hong-ht25tma <ali.arkan@orioninc.com>
2023-05-22 18:17:02 +0300

    new bee test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,5 @@
 package base;
 
-import Pages.HomePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
